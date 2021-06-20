401dc991dbf4f306c480ccb270677e33bad841bd
Martin Hardselius <martin@hardselius.dev>
2021-06-20 09:20:00 +0200

    Implement random diffuse bounce rays

--- src/vec.rs
+++ src/vec.rs
@@ -1,6 +1,9 @@
+use rand::prelude::*;
 use std::fmt;
 use std::fmt::Display;
-use std::ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign};
+use std::ops::{
+    Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Range, Sub, SubAssign,
+};
 
 #[derive(Clone, Copy)]
 pub struct Vec3 {
@@ -13,6 +16,27 @@ pub type Color = Vec3;
 impl Vec3 {
     pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
         Vec3 { e: [e0, e1, e2] }
+    }
+
+    pub fn random(r: Range<f64>) -> Vec3 {
+        let mut rng = rand::thread_rng();
+
+        Vec3 {
+            e: [
+                rng.gen_range(r.clone()),
+                rng.gen_range(r.clone()),
+                rng.gen_range(r),
+            ],
+        }
+    }
+
+    pub fn random_in_unit_sphere() -> Vec3 {
+        loop {
+            let v = Vec3::random(-1.0..1.0);
+            if v.length() < 1.0 {
+                return v;
+            }
+        }
     }
 
     pub fn x(self) -> f64 {
