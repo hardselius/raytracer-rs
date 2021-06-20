3d1198dee7b8557091ba22043c59755c3838d19b
Martin Hardselius <martin@hardselius.dev>
2021-06-20 20:48:08 +0200

    Add metal material

--- src/material.rs
+++ src/material.rs
@@ -29,3 +29,26 @@ impl Scatter for Lambertian {
         Some((self.albedo, scattered))
     }
 }
+
+pub struct Metal {
+    albedo: Color,
+}
+
+impl Metal {
+    pub fn new(a: Color) -> Self {
+        Self { albedo: a }
+    }
+}
+
+impl Scatter for Metal {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+        let reflected = r_in.direction().reflect(rec.normal).normalized();
+        let scattered = Ray::new(rec.p, reflected);
+
+        if scattered.direction().dot(rec.normal) > 0.0 {
+            Some((self.albedo, scattered))
+        } else {
+            None
+        }
+    }
+}
