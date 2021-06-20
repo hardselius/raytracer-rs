40710b313afbaec606fd5b686134f1f82cff2fe3
Martin Hardselius <martin@hardselius.dev>
2021-06-20 20:49:29 +0200

    Reference Self in Vec3 implementation

--- src/vec.rs
+++ src/vec.rs
@@ -14,8 +14,8 @@ pub type Point3 = Vec3;
 pub type Color = Vec3;
 
 impl Vec3 {
-    pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
-        Vec3 { e: [e0, e1, e2] }
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
+        Self { e: [e0, e1, e2] }
     }
 
     pub fn random(r: Range<f64>) -> Vec3 {
